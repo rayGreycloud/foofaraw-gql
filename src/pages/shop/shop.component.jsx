dbc8712868af88a079dde88b423cb822fa3cda01
rayGreycloud <rayGreycloud@gmail.com>
2019-09-17 11:54:49 -0700

    Refactor for gql

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.gql';
-import CollectionPage from '../collection/collection.component';
+import CollectionPage from '../collection/collection.gql';
 
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
