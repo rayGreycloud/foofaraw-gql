ec3c86d5caddb28943b8797970dff2bf1d1049c9
rayGreycloud <rayGreycloud@gmail.com>
2019-09-18 05:59:32 -0700

    Refactor cart hidden w/gql

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-boost';
 
+import { resolvers, typeDefs } from './graphql/resolvers';
 import { store, persistor } from './redux/store';
 
 import './index.css';
@@ -21,7 +22,15 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  resolvers,
+  typeDefs
+});
+
+client.writeData({
+  data: {
+    cartHidden: true
+  }
 });
 
 ReactDOM.render(
